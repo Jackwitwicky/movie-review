53e94833a538e6ee63d0eb61e655ef3578a75ba3
Jackwitwicky <jacknkiarie@gmail.com>
2015-09-15 19:11:55 +0300

    Done adding movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,51 @@
+class MoviesController < ApplicationController
+  def index
+    @movie = Movie.all
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      flash[:success] = "Your movie has been created"
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+
+    if @movie.update(movie_params)
+      flash[:success] = "Your movie has been edited"
+      redirect_to @movie
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+
+    flash[:info] = "Your movie has been deleted"
+    redirect_to root_path
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :movie_length, :director, :rating)
+  end
+end
