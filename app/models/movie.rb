9cc9892f95e106efba4877b10d5b37a41e2c0de5
Jackwitwicky <jacknkiarie@gmail.com>
2015-09-21 09:08:48 +0300

    Done adding image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
+
+  has_attached_file :image, styles: { medium: "400x600#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
