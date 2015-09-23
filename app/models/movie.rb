018947d8af09d306741ae51969a3eb78a6045e90
Jackwitwicky <jacknkiarie@gmail.com>
2015-09-23 19:21:59 +0300

    Done adding some styling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
 
-  has_attached_file :image, styles: { medium: "400x600#" }
+  has_attached_file :image, styles: { medium: "300x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
